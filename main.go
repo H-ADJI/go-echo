102886960b45eec5162c17a9d93d44360545e861
khalil hadji <h-adji_tech@proton.me>
2025-05-07 23:14:08 +0200

    basic imcomp impl

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
-func main(){
-
+func main() {
 	fmt.Println("Hello world")
+	l, err := net.Listen("tcp", "localhost:9090")
+	if err != nil {
+		log.Fatal("Error creating server", err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		fmt.Println(conn.RemoteAddr())
+		if err != nil {
+			log.Fatal("Error while accepting connection", err)
+		}
+		go HandleConnection(conn)
+	}
+
+}
+func HandleConnection(conn net.Conn) {
+	message := make([]byte, 0)
+	_, err := conn.Read(message)
+	if err != nil {
+		log.Fatal("error while reading from connection")
+	}
+	fmt.Printf("%s \n", message)
 }
