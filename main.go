cbf4fa81d59d1c57003a37ad8caba4277afd9224
khalil hadji <h-adji_tech@proton.me>
2025-05-08 16:16:23 +0200

    working impl

--- main.go
+++ main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net"
 )
 
 func main() {
-	fmt.Println("Hello world")
-	l, err := net.Listen("tcp", "localhost:9090")
-	if err != nil {
-		log.Fatal("Error creating server", err)
-	}
-
+	echoSRV := NewServer()
+	defer echoSRV.listener.Close()
 	for {
-		conn, err := l.Accept()
-		fmt.Println(conn.RemoteAddr())
+		conn, err := echoSRV.listener.Accept()
 		if err != nil {
-			log.Fatal("Error while accepting connection", err)
+			log.Printf("Could not accept connection %s - %s", conn.RemoteAddr(), err)
+			continue
 		}
-		go HandleConnection(conn)
+		go echoSRV.HandleConn(conn)
 	}
-
-}
-func HandleConnection(conn net.Conn) {
-	message := make([]byte, 0)
-	_, err := conn.Read(message)
-	if err != nil {
-		log.Fatal("error while reading from connection")
-	}
-	fmt.Printf("%s \n", message)
 }
